dbaa63a797ed724800910efa97a0a7ab0764e95c
Jacob Hexamer <jacobhexamer@gmail.com>
2024-08-21 17:21:40 +0000

    feat: added username databse search and responses for if found or not

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo "Enter your username:"
+echo -e "\nEnter your username:"
 read USERNAME
+
+# check if user is in database
+USERNAME_RESULT=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$USERNAME'")
+
+# if user doesn't exist
+if [[ -z $USERNAME_RESULT ]]
+then
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+
+  #insert new user
+else
+  echo -e "\n Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
 
 
